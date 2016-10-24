5d876abc6c4253a0b452fe6c932000cd4c04b000
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-10-24 12:58:05 +0200

    Defining first doctrine mappings

---
+++ Entity/Product.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Librinfo\ProductBundle\Entity;
+
+use Librinfo\DoctrineBundle\Entity\Traits\BaseEntity;
+use Librinfo\OuterExtensionBundle\Entity\Traits\OuterExtensible;
+use Sylius\Component\Product\Model\Product as BaseProduct;
+
+class Product extends BaseProduct
+{
+//    use BaseEntity,
+//        OuterExtensible
+//    ;
+}
---
+++ Entity/ProductOption.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Librinfo\ProductBundle\Entity;
+
+use Librinfo\DoctrineBundle\Entity\Traits\BaseEntity;
+use Librinfo\OuterExtensionBundle\Entity\Traits\OuterExtensible;
+use Sylius\Component\Product\Model\ProductOption as BaseProductOption;
+
+class ProductOption extends BaseProductOption
+{
+//    use BaseEntity,
+//        OuterExtensible
+//    ;
+}
