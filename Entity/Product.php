e8a935d7a7f7225d4ceea39ac98f9e54dba11255
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-10-27 17:19:09 +0200

    Added OuterExtensible trait to Product entity class

--- Entity/Product.php
+++ Entity/Product.php
@@ -2,13 +2,10 @@
 
 namespace Librinfo\ProductBundle\Entity;
 
-use Librinfo\DoctrineBundle\Entity\Traits\BaseEntity;
 use Librinfo\OuterExtensionBundle\Entity\Traits\OuterExtensible;
-use Sylius\Component\Product\Model\Product as BaseProduct;
+use Sylius\Component\Core\Model\Product as BaseProduct;
 
 class Product extends BaseProduct
 {
-//    use BaseEntity,
-//        OuterExtensible
-//    ;
+    use OuterExtensible;
 }
