c1ae3858159fa1d594853d26110a2e6326d18cef
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-11-22 17:37:30 +0100

    Created Product Admin

--- DependencyInjection/LibrinfoProductExtension.php
+++ DependencyInjection/LibrinfoProductExtension.php
@@ -2,9 +2,9 @@
 
 namespace Librinfo\ProductBundle\DependencyInjection;
 
+use Librinfo\CoreBundle\DependencyInjection\LibrinfoCoreExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class LibrinfoProductExtension extends Extension
+class LibrinfoProductExtension extends LibrinfoCoreExtension
 {
     /**
      * {@inheritdoc}
@@ -22,7 +22,9 @@ class LibrinfoProductExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-//        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-//        $loader->load('services.yml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('admin.yml');
+
+        $this->mergeParameter('librinfo', $container, __DIR__ . '/../Resources/config');
     }
 }
