145786d6e0f81ac8bfb78814d58b30a44a355206
Romain <romain.sanchez@libre-informatique.fr>
2016-11-23 09:58:12 +0100

    Fixed namespaces after LibrinfoDoctrineBundle/LibrinfoBaseEntitiesBundle merge

--- Entity/ProductOption.php
+++ Entity/ProductOption.php
@@ -2,7 +2,7 @@
 
 namespace Librinfo\ProductBundle\Entity;
 
-use Librinfo\DoctrineBundle\Entity\Traits\BaseEntity;
+use Librinfo\BaseEntitiesBundle\Entity\Traits\BaseEntity;
 use Librinfo\OuterExtensionBundle\Entity\Traits\OuterExtensible;
 use Sylius\Component\Product\Model\ProductOption as BaseProductOption;
 
