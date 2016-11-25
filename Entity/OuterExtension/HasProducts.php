fa939a9b5c89ecae4fa85a045416a4eb8ff70f78
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-11-25 18:32:42 +0100

    Created HasProducts trait and Product entity outer extension

---
+++ Entity/OuterExtension/HasProducts.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * Copyright (C) 2015-2016 Libre Informatique
+ *
+ * This file is licenced under the GNU GPL v3.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Librinfo\ProductBundle\Entity\OuterExtension;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Librinfo\ProductBundle\Entity\Product;
+
+/**
+ * @author Marcos Bezerra de Menezes <marcos.bezerra@libre-informatique.fr>
+ */
+trait HasProducts
+{
+    /**
+     * @var Collection
+     */
+    private $products;
+
+    public function initProducts()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    /**
+     * @param Product $product
+     * @return this
+     */
+    public function addProduct(Product $product)
+    {
+        $this->products->add($product);
+
+        $this->setOwningSideRelation($product);
+
+        return $this;
+    }
+
+    /**
+     * @param Product $product
+     * @return this
+     */
+    public function removeOrganism(Product $product)
+    {
+        $this->products->removeElement($product);
+        return $this;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+}
--- Entity/Product.php
+++ Entity/Product.php
@@ -3,9 +3,10 @@
 namespace Librinfo\ProductBundle\Entity;
 
 use Librinfo\OuterExtensionBundle\Entity\Traits\OuterExtensible;
+use AppBundle\Entity\OuterExtension\LibrinfoProductBundle\ProductExtension;
 use Sylius\Component\Core\Model\Product as BaseProduct;
 
 class Product extends BaseProduct
 {
-    use OuterExtensible;
+    use OuterExtensible, ProductExtension;
 }
