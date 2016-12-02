e0d9761b4b6f7fecdcb2635b63f025687a97f649
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-12-02 16:01:45 +0100

    Renamed ProductAdminConcrete : now ProductAdmin

--- Admin/ProductAdmin.php
+++ Admin/ProductAdmin.php
@@ -10,12 +10,71 @@
 
 namespace Librinfo\ProductBundle\Admin;
 
+use Blast\CoreBundle\Admin\Traits\HandlesRelationsAdmin;
 use Blast\CoreBundle\Admin\CoreAdmin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sylius\Component\Product\Factory\ProductFactoryInterface;
+use Sylius\Component\Product\Model\ProductInterface;
 
 /**
  * @author Marcos Bezerra de Menezes <marcos.bezerra@libre-informatique.fr>
  */
 class ProductAdmin extends CoreAdmin
 {
-    //put your code here
+    use HandlesRelationsAdmin {
+        configureFormFields as configFormHandlesRelations;
+        configureShowFields as configShowHandlesRelations;
+    }
+
+    /**
+     * Configure create/edit form fields
+     *
+     * @param FormMapper
+     */
+    protected function configureFormFields(FormMapper $mapper)
+    {
+        //calls to methods of traits
+        $this->configFormHandlesRelations($mapper);
+    }
+
+
+    /**
+     * Configure Show view fields
+     *
+     * @param ShowMapper $mapper
+     */
+    protected function configureShowFields(ShowMapper $mapper)
+    {
+        // call to aliased trait method
+        $this->configShowHandlesRelations($mapper);
+    }
+
+    /**
+     * @return ProductInterface
+     */
+    public function getNewInstance()
+    {
+        /** @var ProductFactoryInterface $productFactory **/
+        $productFactory = $this->getConfigurationPool()->getContainer()->get('sylius.factory.product');
+
+        /** @var ProductInterface $product */
+        $object = $productFactory->createNew();
+
+        foreach ($this->getExtensions() as $extension) {
+            $extension->alterNewInstance($this, $object);
+        }
+        return $object;
+    }
+
+
+    public function prePersist($product)
+    {
+        parent::prePersist($product);
+
+        $slugGenerator = $this->getConfigurationPool()->getContainer()->get('sylius.generator.slug');
+        $product->setSlug($slugGenerator->generate($product->getName()));
+    }
+
+
 }
