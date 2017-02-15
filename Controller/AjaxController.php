6cc4f87379bc4ab40e458e53de3e0858fc2c4d5c
Romain <romain.sanchez@libre-informatique.fr>
2017-02-15 12:17:14 +0100

    Updated relation mapping for Organism

---
+++ Controller/AjaxController.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * Copyright (C) 2015-2016 Libre Informatique
+ *
+ * This file is licenced under the GNU GPL v3.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Librinfo\EcommerceBundle\Controller;
+
+use Blast\CoreBundle\Controller\CRUDController;
+use Sylius\Component\Product\Model\Product;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author Marcos Bezerra de Menezes <marcos.bezerra@libre-informatique.fr>
+ */
+class ProductCRUDController extends CRUDController
+{
+    /**
+     * Generate product variant, based on product options
+     * @todo !!
+     */
+    public function generateVariantsAction(Request $request)
+    {
+    }
+
+    public function generateProductSlugAction(Request $request)
+    {
+        $name = $request->query->get('name');
+
+        return new JsonResponse([
+            'slug' => $this->get('sylius.generator.slug')->generate($name),
+        ]);
+    }
+
+}
