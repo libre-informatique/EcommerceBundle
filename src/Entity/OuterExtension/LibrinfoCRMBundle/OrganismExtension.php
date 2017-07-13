756b3ccb49151a6547fbfab297fa6f7c2fb56575
PapsOu <vivien.frasca@libre-informatique.fr>
2017-07-13 12:44:13 +0200

    Stylefix

--- src/Entity/OuterExtension/LibrinfoCRMBundle/OrganismExtension.php
+++ src/Entity/OuterExtension/LibrinfoCRMBundle/OrganismExtension.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
- * This file is part of the Blast Project package.
+ * This file is part of the Lisem Project.
  *
  * Copyright (C) 2015-2017 Libre Informatique
  *
- * This file is licenced under the GNU LGPL v3.
+ * This file is licenced under the GNU GPL v3.
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
  */
@@ -815,14 +815,13 @@ trait OrganismExtension
     public function addAddress(AddressInterface $address)
     {
         if (!$this->hasAddress($address)) {
-
-            if($this->isIndividual()) {
-                if($address->getFirstName() === null || $address->getLastName() === null) {
+            if ($this->isIndividual()) {
+                if ($address->getFirstName() === null || $address->getLastName() === null) {
                     $address->setFirstName($this->getFirstName());
                     $address->setLastName($this->getLastName());
                 }
             } else {
-                if($address->getLastName() === null) {
+                if ($address->getLastName() === null) {
                     $address->setLastName($this->getName());
                 }
             }
