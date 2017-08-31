f8d799dd3b90a2609831a274c88bc8c257f2691c
François GERBES <francois.gerbes@libre-informatique.fr>
2017-08-31 17:33:49 +0200

    Style Fix

--- src/Entity/ShippingMethod.php
+++ src/Entity/ShippingMethod.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of the Blast Project package.
+ *
+ * Copyright (C) 2015-2017 Libre Informatique
+ *
+ * This file is licenced under the GNU LGPL v3.
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
 namespace Librinfo\EcommerceBundle\Entity;
 
 use Sylius\Component\Core\Model\ShippingMethod as BaseShippingMethod;
@@ -11,7 +21,7 @@ use Blast\OuterExtensionBundle\Entity\Traits\OuterExtensible;
 
 class ShippingMethod extends BaseShippingMethod
 {
-    use OuterExtensible;//, Nameable, Stringable; //, Guidable;
+    use OuterExtensible; //, Nameable, Stringable; //, Guidable;
 
     public function __toString()
     {
