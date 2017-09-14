cf0b1441fa2971ff6f4031618139b10b355be7a0
François GERBES <francois.gerbes@libre-informatique.fr>
2017-09-14 17:49:41 +0200

    Add Name for Shipping Method (from translation)
    +Some minor cleaning

--- src/Admin/ShippingMethodTranslationAdmin.php
+++ src/Admin/ShippingMethodTranslationAdmin.php
@@ -4,7 +4,6 @@ namespace Librinfo\EcommerceBundle\Admin;
 
 use Blast\CoreBundle\Admin\CoreAdmin;
 
-class ShippingCategoryAdmin extends SyliusGenericAdmin
+class ShippingMethodTranslationAdmin extends SyliusGenericAdmin
 {
-
 }
