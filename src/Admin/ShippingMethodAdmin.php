8372607aecf9ae5151a41821a2f47d5285fda24b
François GERBES <francois.gerbes@libre-informatique.fr>
2017-10-13 11:31:59 +0200

    Add Amount configuration

--- src/Admin/ShippingMethodAdmin.php
+++ src/Admin/ShippingMethodAdmin.php
@@ -13,25 +13,43 @@
 namespace Librinfo\EcommerceBundle\Admin;
 
 use Sonata\AdminBundle\Form\FormMapper;
+use Librinfo\EcommerceBundle\Form\Type\PriceCentsType;
 
 class ShippingMethodAdmin extends SyliusGenericAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
         parent::configureFormFields($formMapper);
-        $formMapper->add(
-            'configuration',
-            'sonata_type_immutable_array',
-            array(
-                'keys' => array(
-                    array('FR_WEB', 'sonata_type_immutable_array', array(
-                        'keys' => array(
-                            array('amount', 'text', array())
-                        )
-                    ))
-                    //
-                )
+        
+        /* @todo: we should never use explicit tab and group name in php code as it may be changed in blast.yml */
+        $formMapper
+            ->tab('form_tab_general')->with('form_group_parameters')
+            /* @todo : add this also in show and list */
+            ->add(
+                'configuration',
+                'sonata_type_immutable_array',
+                array(
+                      'label' => false,
+                      'required' => false,
+                      'keys' => array(
+                          /* @todo: find a way to loop on channel */
+                          array('default', 'sonata_type_immutable_array', array(
+                              //'attr' => array('class' => 'inline-block'),
+                              'keys' => array(
+                                  array('amount', PriceCentsType::class, array())
+                              )
+                          )),
+                          array('FR_WEB', 'sonata_type_immutable_array', array(
+                              //'attr' => array('class' => 'inline-block'),
+                              'keys' => array(
+                                  array('amount', PriceCentsType::class, array())
+                              )
+                          ))
+                          //
+                      )
+                  )
             )
-        );
+            ->end()->end();
+        ;
     }
 }
