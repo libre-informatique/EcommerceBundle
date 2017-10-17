35d6a3144761d05c52107b2df137b7eae6269eca
François GERBES <francois.gerbes@libre-informatique.fr>
2017-10-17 19:32:41 +0200

    Add form for all channel in shipping method

--- src/Admin/ShippingMethodAdmin.php
+++ src/Admin/ShippingMethodAdmin.php
@@ -13,41 +13,56 @@
 namespace Librinfo\EcommerceBundle\Admin;
 
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
 use Librinfo\EcommerceBundle\Form\Type\PriceCentsType;
 
 class ShippingMethodAdmin extends SyliusGenericAdmin
 {
+
+
+    public function genChannelArray(string $sonataType = 'sonata_type_immutable_array')
+    {
+        $channelKeyTab = [];
+
+        foreach ($this->getConfigurationPool()->getContainer()
+         ->get('sylius.repository.channel')->findAll() as $channel) {
+            $channelKeyTab []=
+            [$channel->getCode(), $sonataType, [
+                'keys' => [
+                    ['amount', PriceCentsType::class, []],
+                ],
+            ]];
+        }
+        return $channelKeyTab;
+    }
+
     protected function configureFormFields(FormMapper $formMapper)
     {
         parent::configureFormFields($formMapper);
-
+        
         /* @todo: we should never use explicit tab and group name in php code as it may be changed in blast.yml */
         $formMapper
-            ->tab('form_tab_general')->with('form_group_parameters')
-            /* @todo : add this also in show and list */
-            ->add(
-                'configuration',
-                'sonata_type_immutable_array',
-                array(
-                      'label'    => false,
-                      'required' => false,
-                      'keys'     => array(
-                          /* @todo: find a way to loop on channel */
-                          array('default', 'sonata_type_immutable_array', array(
-                              //'attr' => array('class' => 'inline-block'),
-                              'keys' => array(
-                                  array('amount', PriceCentsType::class, array()),
-                              ),
-                          )),
-                          array('FR_WEB', 'sonata_type_immutable_array', array(
-                              //'attr' => array('class' => 'inline-block'),
-                              'keys' => array(
-                                  array('amount', PriceCentsType::class, array()),
-                              ),
-                          )),
-                      ),
-                  )
-            )
-            ->end()->end();
+        ->tab('form_tab_general')->with('form_group_parameters')
+        /* @todo : add this also in show and list */
+        ->add(
+            'configuration',
+            'sonata_type_immutable_array',
+            ['label'    => false,
+            'required' => false,
+            'keys'     => $this->genChannelArray('sonata_type_immutable_array')
+            ]
+        )
+        ->end()->end();
+    }
+    
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        parent::configureShowFields($showMapper);
+        
+        /* @todo: it look like it is more easy to show multi level array in form than in show ... great sonata */
+        // $showMapper
+        //  ->tab('show_tab_general')->with('show_group_parameters')
+        //   ->add('configuration', 'array', [])
+        //  ->end()->end();
     }
 }
